38d9d473a8338a550a88769febeebfd15aa6eb26
Arjun <kingarjun74280@gmail.com>
2025-01-26 12:03:21 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h4 className="text-4xl font-bold bg-yellow-950 text-white">
+        Hellooooooooo yone
+      </h4>
+      <h4 className= " text-9xl bg-pink-500 text-white">
+        Finally, I did it!
+      </h4>
+    </div>
+  );
+}
+
+export default App;
