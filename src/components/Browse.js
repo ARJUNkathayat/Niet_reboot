227945dc0321188fde56e42f5c6833a864e17adc
Arjun <kingarjun74280@gmail.com>
2025-01-31 21:06:46 +0530

    some ui changes,fetch api data ,create some custom hooks

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -4,43 +4,18 @@ import { useNavigate } from 'react-router-dom'
 import {  signOut } from "firebase/auth";
 import { auth } from '../Utils/fireBase';
 import { removeUser } from '../Utils/userSlice';
-import { API_OPTIONS, logo } from '../Utils/constants';
+import {  logo } from '../Utils/constants';
+import useNowPlayingMovies from '../Hooks/useNowPlayingMovies';
+import BrowseHeader from './BrowseHeader';
 
 const Browse = () => {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const handleSignOut = ()=>{
-    signOut(auth).then(() => {
-      // Sign-out successful.
-      navigate("/")
-      dispatch(removeUser())
-    }).catch((error) => {
-      // An error happened.
-    });
-    
-  }
-const getNowPlayingMovies = async()=>{
-  const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1',API_OPTIONS)
-  const json = await data.json();
-  console.log(json)
-}
 
-useEffect(()=>{
-  getNowPlayingMovies()
-},[])
+useNowPlayingMovies()
 
   const user= useSelector((store)=>store.user)
   return (
     <div>
-      <div className='bg-pink-400 w-full h-24 flex justify-between'>
-        <div className='w-48 pt-1.5'>
-          <img  src={logo}></img> </div>
-        <div className='pt-3 flex'>
-          <img className='w-16 h-16' src='https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6'></img>
-            <h3 className='text-white mt-4 ml-3'>{user?.name}</h3>
-          <button onClick={handleSignOut} className='bg-white h-8 ml-5 mt-5'>Sign Out</button>
-        </div>
-        </div> 
+     <BrowseHeader/>
       
       </div>
   )
---
+++ src/components/BrowseHeader.js
@@ -0,0 +1,38 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import {  signOut } from "firebase/auth";
+import { auth } from '../Utils/fireBase';
+import { removeUser } from '../Utils/userSlice';
+import {  logo } from '../Utils/constants';
+
+
+const BrowseHeader = ()=>{
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const handleSignOut = ()=>{
+      signOut(auth).then(() => {
+        // Sign-out successful.
+        navigate("/")
+        dispatch(removeUser())
+      }).catch((error) => {
+        // An error happened.
+      });      
+    }
+    const user= useSelector((store)=>store.user)
+    return(
+        <div>
+             <div className='bg-pink-400 w-full h-24 flex justify-between'>
+                    <div className='w-48 pt-1.5'>
+                      <img  src={logo}></img> </div>
+                    <div className='pt-3 flex'>
+                      <img className='w-16 h-16' src='https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6'></img>
+                        <h3 className='text-white mt-4 ml-3'>{user?.name}</h3>
+                      <button onClick={handleSignOut} className='bg-white h-8 ml-5 mt-5'>Sign Out</button>
+                    </div>
+                    </div> 
+        </div>
+
+    )
+}
+export default BrowseHeader;
